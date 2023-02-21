5f7ccb2327d26a469cbf1c310955956f95d24e4a
gigi <126017633+screamresonance@users.noreply.github.com>
2023-02-21 11:38:40 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+let firstButton = document.querySelector(".option-one");
+let secondButton = document.querySelector(".option-two");
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionTwoScreen = document.querySelector(".option-two-screen");
+firstButton.onclick=function(){
+	optionOneScreen.style.display = "block";
+};
+
+secondButton.onclick=function(){
+	optionTwoScreen.style.display = "block";
+};
+
+
+
+
+
+let onclick=function(){
+
+};
+
+
+
+let INSERT_VARIABLE;
+onclick=function(){
+
+};
+
+
